181d2c29fa56117eb93db00848b2e2f9e30ec1d1
nieshiming <shiming.nie@aihuishou.com>
2021-07-28 18:41:19 +0800

    chore: rmeove 无用文件

--- build/rollup.dev.js
+++ build/rollup.dev.js
@@ -2,7 +2,6 @@ import path from 'path';
 import config from './rollup.base';
 import server from 'rollup-plugin-serve';
 import { eslint } from 'rollup-plugin-eslint';
-import rollupTypescript from 'rollup-plugin-typescript2';
 
 export default {
   ...config,
@@ -14,11 +13,6 @@ export default {
     //   throwOnError: true,
     //   include: [path.resolve(__dirname, '../src/**/*.js')]
     // }),
-    // rollupTypescript({
-    //   useTsconfigDeclarationDir: true,
-    //   include: [path.resolve(__dirname, '../src/**/*.ts')],
-    //   tsconfig: path.resolve(__dirname, '../tsconfig.json')
-    // })
     ...config.plugins,
     server({
       port: 9527,
